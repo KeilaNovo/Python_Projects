b4ca392e8fc88dcf90f540db4296be254b4878f4
KeilaNovo <15122498+KeilaNovo@users.noreply.github.com>
2026-09-20 14:46:09 +0200

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,10 @@
+def factorial(x):
+    """This is a recursive function
+    to find the factorial of an integer"""
+
+    if x == 1 or x == 0:
+        return 1
+    else:
+        # recursive call to the function
+        return (x * factorial(x-1))
+      
